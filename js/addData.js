40cb753b77077ddb5a23ab1b307ea98ed273d28d
Aanshik <aanshik16@gmail.com>
2023-10-08 22:52:23 +0530

    Deleted some unwanted files

--- js/addData.js
+++
@@ -1,8 +0,0 @@
-// Functions
-function addData() {
-  // Getting the date and time when the item is being added
-  const date = new Date();
-  const day = date.getDate();
-  const month = date.getMonth() + 1;
-  const year = date.getFullYear();
-}
